92dc2675dd76887f3a9bc17961d68bb85e833ec9
Joseph Wong <josephwong17@gmail.com>
2023-01-01 14:23:09 +1100

    New class for testing

---
+++ GUITest.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class GUITest extends Frame {
+
+    public GUITest() {
+        super();
+    }
+    
+    public static void main(String[] args) {
+        GUITest GUI = new GUITest();
+    }
+}
